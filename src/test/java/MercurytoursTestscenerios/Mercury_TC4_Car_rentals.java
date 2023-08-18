cd8da758bbfdd0f9cf9c3fd7286b953a6d14e1bb
Pradeepkumarav <pradeep7725@gmail.com>
2023-08-18 17:16:43 +0530

    yes

--- src/test/java/MercurytoursTestscenerios/Mercury_TC4_Car_rentals.java
+++ src/test/java/MercurytoursTestscenerios/Mercury_TC4_Car_rentals.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 public class Mercury_TC4_Car_rentals {
-
+Hii
 	@Test
 	public void Carrentals(){
 	System.out.println("car rentals");
